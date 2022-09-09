58d7ea41071cb6809a4645a6bd6b726381e33f59
ip99202 <ip99202@naver.com>
2022-09-09 14:37:55 +0900

    feat. strike, ball, notion 판별로직 구현

---
+++ src/main/java/baseball/Ball.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import java.util.Objects;
+
+public class Ball {
+
+    private final int index;
+    private final int ballNum;
+
+    public Ball(int index, int ballNum) {
+        this.index = index;
+        this.ballNum = ballNum;
+    }
+
+    public BallStatus play(Ball ball) {
+        if (this.equals(ball)) {
+            return BallStatus.STRIKE;
+        }
+        if (ball.matchBallNum(ballNum)) {
+            return BallStatus.BALL;
+        }
+        return BallStatus.NOTHING;
+    }
+
+    private boolean matchBallNum(int ballNum) {
+        return this.ballNum == ballNum;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ball ball = (Ball) o;
+        return index == ball.index && ballNum == ball.ballNum;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index, ballNum);
+    }
+}
---
+++ src/test/java/baseball/BallTest.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallTest {
+
+    private Ball computer;
+
+    @BeforeEach
+    void comSetup() {
+        computer = new Ball(3, 8);
+    }
+
+    @Test
+    void strike() {
+        BallStatus status = computer.play(new Ball(3, 8));
+         assertThat(status).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void ball() {
+        BallStatus status = computer.play(new Ball(1, 8));
+        assertThat(status).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void noting() {
+        BallStatus status = computer.play(new Ball(2, 3));
+        assertThat(status).isEqualTo(BallStatus.NOTHING);
+    }
+}
